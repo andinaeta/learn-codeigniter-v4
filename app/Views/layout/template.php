54cee29e0dcc77f5bd7c717b5b3e559860881157
andinaeta <mikorlk4328@gmail.com>
2020-06-18 13:46:44 +0700

    add hyperlink: style.css

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/assets/vendor/bootstrap-4.5.0/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="/assets/css/style.css" />
   <title><?= $title; ?></title>
 </head>
+
 <body>
 
   <!-- Parsial -->
@@ -17,4 +20,5 @@
   <script src="/assets/vendor/popper/popper.min.js"></script>
   <script src="/assets/vendor/bootstrap-4.5.0/js/bootstrap.min.js"></script>
 </body>
+
 </html>
