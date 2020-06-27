93703dc14c85d6935da2a4d1ecab64fb4e02bdb0
andinaeta <mikorlk4328@gmail.com>
2020-06-28 04:10:51 +0700

    codeigniter 4 validation

--- app/Views/komik/create.php
+++ app/Views/komik/create.php
@@ -10,25 +10,37 @@
         <div class="form-group row">
           <label for="judul" class="col-sm-2 col-form-label">Judul</label>
           <div class="col-sm-10">
-            <input type="text" name="judul" id="judul" class="form-control" autofocus>
+            <input type="text" name="judul" id="judul" class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' : ''; ?>" autofocus value="<?= old('judul'); ?>">
+            <div class="invalid-feedback">
+              <?= $validation->getError('judul'); ?>
+            </div>
           </div>
         </div>
         <div class="form-group row">
           <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
           <div class="col-sm-10">
-            <input type="text" name="penulis" id="penulis" class="form-control">
+            <input type="text" name="penulis" id="penulis" class="form-control <?= ($validation->hasError('penulis')) ? 'is-invalid' : ''; ?>" value="<?= old('penulis'); ?>">
+            <div class="invalid-feedback">
+              <?= $validation->getError('penulis'); ?>
+            </div>
           </div>
         </div>
         <div class="form-group row">
           <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
           <div class="col-sm-10">
-            <input type="text" name="penerbit" id="penerbit" class="form-control">
+            <input type="text" name="penerbit" id="penerbit" class="form-control <?= ($validation->hasError('penerbit')) ? 'is-invalid' : ''; ?>" value="<?= old('penerbit'); ?>">
+            <div class="invalid-feedback">
+              <?= $validation->getError('penerbit'); ?>
+            </div>
           </div>
         </div>
         <div class="form-group row">
           <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
           <div class="col-sm-10">
-            <input type="text" name="sampul" id="sampul" class="form-control">
+            <input type="text" name="sampul" id="sampul" class="form-control <?= ($validation->hasError('sampul')) ? 'is-invalid' : ''; ?>" value="<?= old('sampul'); ?>">
+            <div class="invalid-feedback">
+              <?= $validation->getError('sampul'); ?>
+            </div>
           </div>
         </div>
         <div class="form-group row">
